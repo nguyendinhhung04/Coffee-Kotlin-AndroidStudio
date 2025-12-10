b7a1edd8e3b478e7703c3a217990c65cfed0925b
nguyendinhhung04 <dinhhung5a6@gmail.com>
2025-12-11 00:34:44 +0700

    merge favorite and noti

--- app/src/main/java/com/example/coffeeshop/ui/activity/FavoritesActivity.kt
+++ app/src/main/java/com/example/coffeeshop/ui/activity/FavoritesActivity.kt
@@ -29,8 +29,6 @@ class FavoritesActivity : AppCompatActivity() {
     private lateinit var adapter: DrinkItemAdapter
 
     private lateinit var bottomNavigationView: BottomNavigationView
-    private lateinit var ivMenu: ImageView
-    private lateinit var ivNotification: ImageView
 
     private lateinit var ivFavoritesCart: ImageView
 
@@ -44,18 +42,6 @@ class FavoritesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_favorites)
 
         bottomNavigationView = findViewById(R.id.bottom_navigation_favorites)
-        ivMenu = findViewById(R.id.ivFavoritesMenu)
-        ivNotification = findViewById(R.id.ivBell)
-
-        ivMenu.setOnClickListener {
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
-        }
-
-        ivNotification.setOnClickListener {
-            val intent = Intent(this, NotificationActivity::class.java)
-            startActivity(intent)
-        }
 
         setupBottomNavigation()
 
