e765a00f48311607d68118d7cd35de08dfdf516e
vietdung2811 <vietdung281104@gmail.com>
2025-12-17 16:36:04 +0700

    update random bullshit

--- app/src/main/java/com/example/coffeeshop/ui/activity/YourOrderActivity.kt
+++ app/src/main/java/com/example/coffeeshop/ui/activity/YourOrderActivity.kt
@@ -229,8 +229,6 @@ class YourOrderActivity : AppCompatActivity() {
     private fun handleReorder(order: Order) {
         if (order.status != "Delivered") return
 
-        CartManager.clearCart()
-
         order.items.forEach { oi ->
             val item = ItemRepository.getItemById(oi.productId) ?: return@forEach
 
